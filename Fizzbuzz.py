5ff83e68785fc175afb89b57426e5981c33ef63f
alfonso-torres <alfonso_sepul@hotmail.com>
2021-03-25 19:49:24 +0000

    Exercise Fizzbuzz and Readme done.

---
+++ Fizzbuzz.py
@@ -0,0 +1,36 @@
+# Exercise 1 - Fizzbuzz
+
+# Write a program that outputs sequentially the integers from 1 to 100, but on some conditions prints a string instead:
+# when the integer is a multiple of 3 print “Fizz” instead of the number,
+# when it is a multiple of 5 print “Buzz” instead of the number,
+# when it is a multiple of both 3 and 5 print “FizzBuzz” instead of the number.
+
+# Notes: must be in a class and method format
+
+# Let's create the Fizzbuzz class
+class Fizzbuzz:
+
+    def __init__(self):
+        self.three = 3 # Integer to check if is a multiple of 3
+        self.five = 5 # Integer to check if is a multiple of 5
+
+# Function that will check if the number is multiple of 3 or 5 and print the correct answer
+    def fizzbuzz_prints(self):
+        i = 1
+        while i <= 100: # loop while from 1 to 100
+            if i % self.three == 0 and i % self.five == 0: # check if is multiple of both
+                print("FizzBuzz")
+            elif i % self.three == 0: # check if is multiple of 3
+                print("Fizz")
+            elif i % self.five == 0: # check if is multiple of 5
+                print("Buzz")
+            else:
+                print(i) # number that is not dividable by 3 or 5
+
+            i += 1
+
+# Let's create an object of the Fizzbuzz Class
+object_fizzbuzz = Fizzbuzz()
+
+# Call the function that will print the results
+object_fizzbuzz.fizzbuzz_prints()
--- README.md
+++ README.md
@@ -1,1 +1,38 @@
 # OOP Exercises
+
+## Exercise 1: Fizzbuzz
+
+*Tasks*:
+
+- Write a program that prints numbers from 1 to 100.
+- For multiples of three print "Fizz" instead of the number.
+- For multiples of five print "Buzz".
+- For numbers which are multiples of both three and five print "FizzBuzz"
+
+*Note*:
+
+- Must be in a class and method format.
+
+*Results*:
+
+The code related of this exercise is in the file `Fizzbuzz.py`.
+
+## Exercise 2: Scrabble word calculator
+
+*Task*:
+
+- Given the below scoring create a Scrabble word calculator that will provide
+the correct scores dependent on the string provided.
+````buildoutcfg
+Letter -> Value
+A, E, I, O, U, L, N, R, S, T -> 1
+D, G -> 2
+B, C, M, P -> 3
+F, H, V, W, Y -> 4
+K -> 5
+J, X -> 8
+Q, Z -> 10
+````  
+*Results*:
+
+The code related of this exercise is in the file `Fizzbuzz.py`.
